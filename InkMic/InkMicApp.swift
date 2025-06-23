42d50ea5e8a89737a48421f8c5a6437217dc3d8d
MO HUAIHAO <mohuaihaoxm@163.com>
2025-06-23 17:47:52 +0800

    Initial Commit

---
+++ InkMic/InkMicApp.swift
@@ -0,0 +1,17 @@
+//
+//  InkMicApp.swift
+//  InkMic
+//
+//  Created by Huaihao on 6/23/25.
+//
+
+import SwiftUI
+
+@main
+struct InkMicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InkMicTests/InkMicTests.swift
@@ -0,0 +1,17 @@
+//
+//  InkMicTests.swift
+//  InkMicTests
+//
+//  Created by Huaihao on 6/23/25.
+//
+
+import Testing
+@testable import InkMic
+
+struct InkMicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
